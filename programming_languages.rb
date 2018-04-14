9a0dbe6b7eab2be79c7cb871179f36b256f4f265
Ryan Ghaida <ryaboug@gmail.com>
2018-04-14 19:14:12 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,82 @@
-def reformat_languages(languages)
-  # your code here
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+
+def reformat_languages (languages)
+  
+  new_hash = {}
+  arr = []
+  
+  
+  languages.collect do |first_tier, language|
+    
+    if first_tier == :oo 
+      
+      puts ":oo" 
+      
+      language.collect do |lang, type|
+      
+        puts new_hash[lang] = type
+        
+        
+      end
+      
+      
+    else 
+      
+      puts ":functional"
+      
+        language.collect do |lang, type|
+      
+        puts new_hash[lang] = type
+        
+      end 
+    end 
+  end
+
+
+new_hash
+  
 end
+
+reformat_languages(languages)
+
+
+
+    
+
+
+
+
+
+
+
