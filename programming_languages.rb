fdda91c1e8ff635e7f6ea97c3d9d929817d4e2d6
Ryan Ghaida <ryaboug@gmail.com>
2018-04-14 19:14:20 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -26,57 +26,44 @@ languages = {
     :javascript => {
       :type => "interpreted"
     }
- 
+
   }
 }
 
 
 def reformat_languages (languages)
-  
+
   new_hash = {}
   arr = []
-  
-  
+
+
   languages.collect do |first_tier, language|
-    
-    if first_tier == :oo 
-      
-      puts ":oo" 
-      
+
+    if first_tier == :oo
+
+      puts ":oo"
+
       language.collect do |lang, type|
-      
+
         puts new_hash[lang] = type
-        
-        
+
+
       end
-      
-      
-    else 
-      
+
+
+    else
+
       puts ":functional"
-      
+
         language.collect do |lang, type|
-      
+
         puts new_hash[lang] = type
-        
-      end 
-    end 
+
+      end
+    end
   end
 
 
 new_hash
-  
+
 end
-
-reformat_languages(languages)
-
-
-
-    
-
-
-
-
-
-
-
